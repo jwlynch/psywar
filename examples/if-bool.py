c055725cd6acedefd21ea64ab771c122d8e52aae
Jim <jim@jam.sessionsnet.org>
2019-02-27 14:12:36 -0800

    simple if with const boolean in a var

---
+++ examples/if-bool.py
@@ -0,0 +1,13 @@
+# it's good to comment your code so you understand it
+
+# here, we're using a variable gets_toaster_p which is True
+# if person gets a toaster
+
+gets_toaster_p = True
+
+if gets_toaster_p:
+    print("congratulations!")
+    print("you get a toaster!")
+print("thanks for using our bank")
+print("enjoy the rest of your day")
+
